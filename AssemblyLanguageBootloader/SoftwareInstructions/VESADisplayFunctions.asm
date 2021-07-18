7a2819c51ef303a590728c1acbfb03b0eb92dbe6
ericatol <atolbirdmaker@gmail.com>
2021-07-17 19:34:59 -0700

    Fixed inaccurate wording

--- AssemblyLanguageBootloader/SoftwareInstructions/VESADisplayFunctions.asm
+++ AssemblyLanguageBootloader/SoftwareInstructions/VESADisplayFunctions.asm
@@ -5,7 +5,7 @@ VESAClearScreen:
 
     mov edi, [5028h]                            ; Physical address base pointer (Frame buffer) of mode info block set during "Post Boot Setup"
     mov ecx, 1980*1080                          ; ARGB 32 bytes per pixel (bpp)
-    mov eax, 0000FFh                            ; ARGB coloring [Alpha|Red|Green|Blue] 2 bits per set, EG: 0000FFh=blue
+    mov eax, 0000FFh                            ; ARGB coloring [Alpha|Red|Green|Blue] 8 bits per set / 1 byte per set, EG: 0000FFh=11111111=255=blue
     rep stosd
 
     hlt ; change to not halt
